158383783aef064e2768fd920eb28924fb101c28
Valentin BEYNARD <43931090+valentinBeynard@users.noreply.github.com>
2019-06-04 23:19:41 +0200

    Original Version

---
+++ Signal_Detector/Debug/Drivers/BSP/Components/ft5336/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/Components/ft5336/ft5336.c 
+
+OBJS += \
+./Drivers/BSP/Components/ft5336/ft5336.o 
+
+C_DEPS += \
+./Drivers/BSP/Components/ft5336/ft5336.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/Components/ft5336/ft5336.o: ../Drivers/BSP/Components/ft5336/ft5336.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F746xx -DDEBUG -c -I../Inc -I"C:/Users/BEYNARD/Documents/GitHub/-CPE-Lyon-ESE-Signal_Detector/Signal_Detector/Drivers/BSP/Components" -I../Drivers/CMSIS/Include -I"C:/Users/BEYNARD/Documents/GitHub/-CPE-Lyon-ESE-Signal_Detector/Signal_Detector/Drivers/BSP" -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I"C:/Users/BEYNARD/Documents/GitHub/-CPE-Lyon-ESE-Signal_Detector/Signal_Detector/Utilities/Fonts" -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FatFs/src -I../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/BSP/Components/ft5336/ft5336.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
---
+++ Signal_Detector/Debug/sources.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Drivers/BSP/Components/adv7533 \
+Drivers/BSP/Components/exc7200 \
+Drivers/BSP/Components/ft5336 \
+Drivers/BSP/Components/ft6x06 \
+Drivers/BSP/Components/mfxstm32l152 \
+Drivers/BSP/Components/otm8009a \
+Drivers/BSP/Components/ov9655 \
+Drivers/BSP/Components/s5k5cag \
+Drivers/BSP/Components/st7735 \
+Drivers/BSP/Components/st7789h2 \
+Drivers/BSP/Components/stmpe811 \
+Drivers/BSP/Components/ts3510 \
+Drivers/BSP/Components/wm8994 \
+Drivers/BSP \
+Drivers/STM32F7xx_HAL_Driver/Src \
+Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src \
+Middlewares/ST/STM32_USB_Host_Library/Core/Src \
+Middlewares/Third_Party/FatFs/src \
+Middlewares/Third_Party/FatFs/src/option \
+Src \
+Startup \
+
